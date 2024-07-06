7e52099a338609a265f370b2ab7af07b771f81f3
Roman Sharkov <roman.scharkov@gmail.com>
2024-07-06 14:27:28 +0200

    test: Add tests

--- internal/broadcaster/broadcaster.go
+++ internal/broadcaster/broadcaster.go
@@ -11,18 +11,30 @@ func NewSignalBroadcaster() *SignalBroadcaster {
 	return &SignalBroadcaster{listeners: map[chan<- struct{}]struct{}{}}
 }
 
+// Len returns the number of listeners.
+func (b *SignalBroadcaster) Len() int {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	return len(b.listeners)
+}
+
+// AddListener adds channel c to listeners,
+// which will be written to once BroadcastNonblock is invoked.
 func (b *SignalBroadcaster) AddListener(c chan<- struct{}) {
 	b.lock.Lock()
 	defer b.lock.Unlock()
 	b.listeners[c] = struct{}{}
 }
 
+// RemoveListener removes channel c from the listeners.
 func (b *SignalBroadcaster) RemoveListener(c chan<- struct{}) {
 	b.lock.Lock()
 	defer b.lock.Unlock()
 	delete(b.listeners, c)
 }
 
+// BroadcastNonblock writes to all listeners in a non-blocking manner.
+// BroadcastNonblock ignores unresponsive listeners.
 func (b *SignalBroadcaster) BroadcastNonblock() {
 	b.lock.Lock()
 	defer b.lock.Unlock()
---
+++ internal/broadcaster/broadcaster_test.go
@@ -0,0 +1,57 @@
+package broadcaster_test
+
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"github.com/romshark/templier/internal/broadcaster"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBroadcast(t *testing.T) {
+	b := broadcaster.NewSignalBroadcaster()
+	require.Equal(t, 0, b.Len())
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	var prepare sync.WaitGroup
+	prepare.Add(2)
+	var removed sync.WaitGroup
+	removed.Add(2)
+
+	var counter atomic.Int32
+
+	go func() {
+		defer wg.Done()
+		c := make(chan struct{}, 1)
+		b.AddListener(c)
+		prepare.Done()
+		<-c
+		counter.Add(1)
+		b.RemoveListener(c)
+		removed.Done()
+	}()
+
+	go func() {
+		defer wg.Done()
+		c := make(chan struct{}, 1)
+		b.AddListener(c)
+		prepare.Done()
+		<-c
+		counter.Add(1)
+		b.RemoveListener(c)
+		removed.Done()
+	}()
+
+	prepare.Wait()
+	require.Equal(t, 2, b.Len())
+	b.BroadcastNonblock()
+	wg.Wait()
+	require.Equal(t, int32(2), counter.Load())
+
+	removed.Wait()
+	require.Equal(t, 0, b.Len())
+	b.BroadcastNonblock()
+	require.Equal(t, int32(2), counter.Load())
+}
