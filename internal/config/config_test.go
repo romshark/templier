78e6196e04a05fc8eceaf72995e1c313a5b2d354
Roman Sharkov <roman.scharkov@gmail.com>
2025-01-13 16:52:39 +0100

    test: Use f-tests

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -16,45 +16,29 @@ func TestValidateType(t *testing.T) {
 }
 
 func TestSpaceSeparatedList(t *testing.T) {
+	t.Parallel()
+
 	type TestConfig struct {
 		List config.SpaceSeparatedList `yaml:"list"`
 	}
-	for _, td := range []struct {
-		name   string
-		input  string
-		expect config.SpaceSeparatedList
-	}{
-		{
-			name:   "empty",
-			input:  "list:",
-			expect: config.SpaceSeparatedList(nil),
-		},
-		{
-			name:   "empty_explicit",
-			input:  "list: ''",
-			expect: config.SpaceSeparatedList(nil),
-		},
-		{
-			name:   "single",
-			input:  "list: one_item",
-			expect: config.SpaceSeparatedList{"one_item"},
-		},
-		{
-			name:   "spaces",
-			input:  "list: -flag value",
-			expect: config.SpaceSeparatedList{"-flag", "value"},
-		},
-		{
-			name:   "multiline",
-			input:  "list: |\n  first second\n  third\tfourth",
-			expect: config.SpaceSeparatedList{"first", "second", "third", "fourth"},
-		},
-	} {
-		t.Run(td.name, func(t *testing.T) {
-			var actual TestConfig
-			err := yamagiconf.Load(td.input, &actual)
-			require.NoError(t, err)
-			require.Equal(t, td.expect, actual.List)
-		})
+
+	f := func(t *testing.T, input string, expect config.SpaceSeparatedList) {
+		t.Helper()
+		var actual TestConfig
+		err := yamagiconf.Load(input, &actual)
+		require.NoError(t, err)
+		require.Equal(t, expect, actual.List)
 	}
+
+	// Empty.
+	f(t, "list:", config.SpaceSeparatedList(nil))
+	// Empty explicit.
+	f(t, "list: ''", config.SpaceSeparatedList(nil))
+	// Single.
+	f(t, "list: one_item", config.SpaceSeparatedList{"one_item"})
+	// Spaces.
+	f(t, "list: -flag value", config.SpaceSeparatedList{"-flag", "value"})
+	// Multiline.
+	f(t, "list: |\n  first second\n  third\tfourth",
+		config.SpaceSeparatedList{"first", "second", "third", "fourth"})
 }
